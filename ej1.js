f1d2551ccf4195b2132192d4f391e2063e40b74b
Lio <liobensignor@gmail.com>
2024-09-04 23:43:36 -0300

    Primer Parcial

---
+++ ej1.js
@@ -0,0 +1,31 @@
+let nombre;
+let edad;
+let genero;
+
+// genero = m
+// genero == m
+// genero === m
+
+// Banner
+// if edad >= 18 && genero === 'f'
+// if edad >= 18 && genero === 'm'
+// if edad < 18 && genero === 'f'
+// if edad < 18 && genero === 'm'
+// if edad < 0 || edad > 105 || !edad || isNan(edad)
+//    banner5
+//    pastillas mix
+//    color es tal
+
+// Juan para vos
+// if nombre === "" || nombre === numero
+
+// Las pastillas del abuelo
+// if edad >= 18
+// if edad >= 13 && edad <= 17
+// if edad < 13
+// if edad < 0 || edad > 105 || !edad || isNan(edad)
+
+// if genero === 'f'
+// if genero === 'm'
+
+// if alguno invalid -> alert
---
+++ ej2.js
@@ -0,0 +1,25 @@
+// Duelo de titanes
+
+// Reglas y Partida
+// alert:  hacer los ingresos solicitados por turnos sin que uno vea lo que ingresa el otro
+// alert: turno j1
+// prompt: numero del 1 al 10
+// prompt: color violeta o naranja ("v" || "n")
+// alert: turno j2
+// prompt: numero del 1 al 10
+// prompt: color violeta o naranja ("v" || "n")
+// if (!numero || isNan(numero)) || (color !== "v" || color !== "n")
+//    el juego no se pudo realizar porque no se ingresaron los datos correctamente
+
+// else Ganador
+// if n1 > n2 -> gana j1
+// if n2 > n1 -> gana j2
+// if n1 === n2 && c1 === "n" -> gana j1
+// if n1 === n2 && c2 === "n" -> gana j2
+// if n1 === n2 && c1 === c2 -> empate
+
+// p n1
+// p n2
+// p c1
+// p c2
+// p resultado
